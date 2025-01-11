4e9c9276d385dfc54473ec0f3f29cc13d4d311a2
iwolfking <thegreatiwk@gmail.com>
2025-01-11 10:17:17 -0600

    Wolds compat and fixes offering pillar crash

--- src/main/java/xyz/iwolfking/unobtainium/integration/VHAPIConfigRegistration.java
+++ src/main/java/xyz/iwolfking/unobtainium/integration/VHAPIConfigRegistration.java
@@ -1,6 +1,7 @@
 package xyz.iwolfking.unobtainium.integration;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.loading.LoadingModList;
 import xyz.iwolfking.unobtainium.Unobtanium;
 import xyz.iwolfking.vhapi.VHAPI;
 import xyz.iwolfking.vhapi.api.events.VHAPIProcessorsEvent;
@@ -18,12 +19,18 @@ public class VHAPIConfigRegistration {
 
     public static void addManualConfigs() {
         Unobtanium.LOGGER.info("Registering Vault config patches!");
+        if(LoadingModList.get().getModFileById("woldsvaults") != null) {
+            Unobtanium.LOGGER.info("Wold's Vaults is present, disabling VHAPI configs from loading.");
+            return;
+        }
+
         registerManualConfigFile("/config_patches/guardian_thorns_patch.json", Unobtanium.id("vault/mobs/guardian_thorns_patch"));
         registerManualConfigFile("/config_patches/overgrown_tank_reach.json", Unobtanium.id("vault/mobs/overgrown_tank_reach_patch"));
         registerManualConfigFile("/config_patches/vault_dweller_speed_fix.json", Unobtanium.id("vault/mobs/vault_dweller_speed_patch"));
         registerManualConfigFile("/config_patches/more_mobs_brazier_fix.json", Unobtanium.id("vault/modifier_pools/more_mobs_brazier_patch"));
         registerManualConfigFile("/config_patches/scavenger_missing_mobs_fix.json", Unobtanium.id("objectives/scavenger/missing_mobs_patch"));
         registerManualConfigFile("/config_patches/gemstone_recycle_rate_patch.json", Unobtanium.id("vault_recycler/gemstone_recycle_patch"));
+
     }
 
 
