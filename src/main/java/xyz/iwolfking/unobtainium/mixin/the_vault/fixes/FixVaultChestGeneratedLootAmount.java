3e889c791ff92ded256c73ddb058b49d8df04749
radimous <jurda92@gmail.com>
2025-01-26 15:26:18 +0100

    fix chest sizes

--- src/main/java/xyz/iwolfking/unobtainium/mixin/the_vault/fixes/FixVaultChestGeneratedLootAmount.java
+++ src/main/java/xyz/iwolfking/unobtainium/mixin/the_vault/fixes/FixVaultChestGeneratedLootAmount.java
@@ -1,30 +1,31 @@
 package xyz.iwolfking.unobtainium.mixin.the_vault.fixes;
 
 
-import org.objectweb.asm.Opcodes;
+import iskallia.vault.init.ModBlocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import iskallia.vault.block.entity.VaultChestTileEntity;
-import net.minecraft.world.Container;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
-@Mixin(value = VaultChestTileEntity.class)
-public class FixVaultChestGeneratedLootAmount
-{
-    @Redirect(method = "fillLoot", at = @At(value = "FIELD", target = "Liskallia/vault/block/entity/VaultChestTileEntity;size:I", opcode = Opcodes.GETFIELD), remap = false)
-    public int fixGeneratedLootSlots(VaultChestTileEntity instance)
-    {
-        // All generated vault chest inventories should be 27 items inside it.
-        return 27;
-    }
-
-
-    @Redirect(method = "getAvailableSlots", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Container;getContainerSize()I"))
-    public int fixFilledSlots(Container instance)
-    {
-        // All generated vault chest inventories should be 27 items inside it.
-        return 27;
+@Mixin(value = VaultChestTileEntity.class, remap = false)
+public class FixVaultChestGeneratedLootAmount {
+    @Inject(method = "getSize", at = @At("HEAD"), cancellable = true)
+    private void getSize(BlockState state, CallbackInfoReturnable<Integer> cir) {
+        Block block = state.getBlock();
+        if (block == ModBlocks.WOODEN_CHEST
+            || block == ModBlocks.LIVING_CHEST
+            || block == ModBlocks.ORNATE_CHEST
+            || block == ModBlocks.GILDED_CHEST
+            || block == ModBlocks.FLESH_CHEST
+            || block == ModBlocks.HARDENED_CHEST
+            || block == ModBlocks.ENIGMA_CHEST
+        ) {
+            cir.setReturnValue(27);
+        }
     }
 }
