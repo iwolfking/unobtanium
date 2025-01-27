2e9d9cea5b8c0bdf9d0e6511dc21fa9ceafe70ba
BONNe <bonne@bonne.id.lv>
2025-01-27 18:54:39 +0200

    Small card and deck memory footprint optimization.
    For some reason deck data stores a lot of empty cards. The code itself allows cards to be null, but updating cards for some reason used empty item stack to get cards, instead of returning null.

---
+++ src/main/java/xyz/iwolfking/unobtainium/mixin/the_vault/optimizations/OptimizeCardDeckContainer.java
@@ -0,0 +1,65 @@
+//
+// Created by BONNe
+// Copyright - 2025
+//
+
+
+package xyz.iwolfking.unobtainium.mixin.the_vault.optimizations;
+
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import iskallia.vault.container.inventory.CardDeckContainer;
+import iskallia.vault.core.card.Card;
+import iskallia.vault.item.CardItem;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+
+
+@Mixin(CardDeckContainer.class)
+public class OptimizeCardDeckContainer
+{
+    @Shadow(remap = false)
+    @Final
+    private ItemStack delegate;
+
+    @Redirect(method = "<init>",
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/nbt/CompoundTag;contains(Ljava/lang/String;)Z"))
+    private boolean removeInventoryTag(CompoundTag instance, String inventory)
+    {
+        // Remove inventory tag. It should be there regardless, as getSharedTag removes "inventory"
+        // but just in case, remove it to avoid any possible issues with actual cards being added
+        // at the end.
+        instance.remove(inventory);
+        return false;
+    }
+
+
+    @Redirect(method = "setChanged",
+        at = @At(value = "INVOKE",
+            target = "Liskallia/vault/item/CardItem;getCard(Lnet/minecraft/world/item/ItemStack;)Liskallia/vault/core/card/Card;"),
+        remap = false)
+    private Card ignoreAirItems(ItemStack stack)
+    {
+        // This will reduce memory consumption of decks, as will allow empty decks to be emtpy.
+        return stack.isEmpty() ? null : CardItem.getCard(stack);
+    }
+
+
+    @Inject(method = "setChanged",
+        at = @At(value = "INVOKE",
+            target = "Liskallia/vault/item/CardDeckItem;setCardDeck(Lnet/minecraft/world/item/ItemStack;Liskallia/vault/core/card/CardDeck;)V"),
+        remap = false)
+    private void removeInventoryTag(CallbackInfo ci)
+    {
+        // This tag is not used at all and is removed in other places. No need to save it.
+        this.delegate.getOrCreateTag().remove("inventory");
+    }
+}
