a20893455bf655a5185cea0217e4138a9deedb8b
iwolfking <thegreatiwk@gmail.com>
2025-07-23 09:24:59 -0500

    U19 compat

--- src/main/java/xyz/iwolfking/unobtainium/mixin/the_vault/fixes/FixRandomJewelCraftingRecipe.java
+++ src/main/java/xyz/iwolfking/unobtainium/mixin/the_vault/fixes/FixRandomJewelCraftingRecipe.java
@@ -7,6 +7,9 @@
 package xyz.iwolfking.unobtainium.mixin.the_vault.fixes;
 
 
+import iskallia.vault.item.core.DataInitializationItem;
+import iskallia.vault.item.core.DataTransferItem;
+import iskallia.vault.item.core.VaultLevelItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,9 +22,6 @@ import iskallia.vault.VaultMod;
 import iskallia.vault.container.oversized.OverSizedItemStack;
 import iskallia.vault.core.random.JavaRandom;
 import iskallia.vault.gear.crafting.recipe.JewelCraftingRecipe;
-import iskallia.vault.item.gear.DataInitializationItem;
-import iskallia.vault.item.gear.DataTransferItem;
-import iskallia.vault.item.gear.VaultLevelItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
